94c51de31e1369707d7a262d5bf3f57df8770207
EC2 Default User <ec2-user@ip-172-31-15-200.us-east-2.compute.internal>
2024-06-06 17:49:23 +0000

    Goofing Off

--- src/App.tsx
+++ src/App.tsx
@@ -46,23 +46,6 @@ function App() {
     <main>
       <h2>{user?.signInDetails?.loginId}'s todos</h2>
       <button onClick={createTodo}>+ new</button>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-      
       <ul>
         {todos.map((todo) => (
 
