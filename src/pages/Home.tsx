8792ae34736def225261f79951d6ffab0426d850
EC2 Default User <ec2-user@ip-172-31-15-200.us-east-2.compute.internal>
2024-06-07 15:47:36 +0000

    Fixing width issues

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home page!</p>
+    <div className="home-container">
+      <h2 className="home-title">Welcome to the Home Page</h2>
+      <div className="home-content">
+        I've got a lovely bunch of coconuts.
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
